534d5cf09f73cc2083d8a8f8810fa748ac3f63ca
totz <yuta.totz@gmail.com>
2014-11-01 20:56:19 +0900

    Create OAuth login of twitter

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,14 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'users/show/:username' => 'users#show'
 
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/logout' => 'sessions#destroy', as: :logout
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'notes#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
