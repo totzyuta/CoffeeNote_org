ebe234f4054a5ceb83fd913c866bedd7d51fc59d
totzYuta <zfbfmtmxaz@gmail.com>
2014-11-01 20:30:45 +0900

    Creating OAuth system

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,6 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
+    session[:user_id] = user.id
+    redirect_to root_path, notice: 'Successfully Logged In'
+end
