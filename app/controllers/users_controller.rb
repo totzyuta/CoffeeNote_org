37ac4a9040d9eb906379d6604b184412b09e06e1
totz <yuta.totz@gmail.com>
2014-12-31 17:27:13 +0900

    Designed users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+
+  before_action :set_current_user
+
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
   def index
     @users = User.all
   end
